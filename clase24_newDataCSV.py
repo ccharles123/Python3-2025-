cdae5f8113c6c95f39fbfdc526e4afdedefb0dbe
Carlos Hernandez <ccharles123@hotmail.com>
2025-10-26 08:27:08 -0500

    egrgando modificaciones en la clase 24

---
+++ clase24_newDataCSV.py
@@ -0,0 +1,15 @@
+import csv
+
+new_product = {
+    'name': 'Wireless Chatger',
+    'price': '78',
+    'quantity': 110,
+    'brand':'ChargerMaster',
+    'category': 'Accessories',
+    'entry_date': '2025-10-26'
+}
+
+with open('products.csv', mode='a', newline='') as file:
+    file.write('\n')
+    csv_write = csv.DictWriter(file, fieldnames = new_product.keys())
+    csv_write.writerow(new_product)
